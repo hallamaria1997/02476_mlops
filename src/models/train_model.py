62ed1643f2ebc75da2543c7a6ad3ad46ce61f25b
hallamh16 <50322630+hallamh16@users.noreply.github.com>
2023-01-12 16:03:53 +0100

    working training loop

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,14 +1,15 @@
+from transformers import AutoModelForSequenceClassification, AutoConfig, AutoTokenizer
 import click
 import matplotlib.pyplot as plt
 import torch
 from torch import nn
 from tqdm import tqdm
 from typing import Callable, Tuple, Union, Optional, List
+from scipy.special import softmax
+import numpy as np
 
-
-
-from src.models.load_data import make_dataloader
-from src.models.model import  SentimentModel    
+from load_data import make_dataloader
+from model import  SentimentModel    
 
 
 @click.group()
@@ -17,12 +18,24 @@ def cli():
 
 
 @click.command()
-@click.option("--lr", default=3e-3, help="learning rate to use for training")
-@click.option("--epochs", default=50, help="number of epochs")
+@click.option("--lr", default=1e-4, help="learning rate to use for training")
+@click.option("--epochs", default=30, help="number of epochs")
 def train(lr:float, epochs:int)->None:
     """main training function for the model, calls the subsequent training function"""
+    
+    def preprocess(text):
+        new_text = []
+        for t in text.split(" "):
+            t = '@user' if t.startswith('@') and len(t) > 1 else t
+            t = 'http' if t.startswith('http') else t
+            new_text.append(t)
+        return " ".join(new_text)
+    
+    
+    tokenizer = AutoTokenizer.from_pretrained( "cardiffnlp/twitter-roberta-base-sentiment-latest")
+    config = AutoConfig.from_pretrained( "cardiffnlp/twitter-roberta-base-sentiment-latest")
     model = SentimentModel()
-    train_set, _ = make_dataloader(data_path="data/processed/train_csv")
+    train_set = make_dataloader(filepath="C:/Users/Lenovo/Documents/02476_mlops/data/raw/train.csv")
     criterion = nn.NLLLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
@@ -33,14 +46,17 @@ def train(lr:float, epochs:int)->None:
         cum_loss = 0
         for tweets, labels in train_set:
             optimizer.zero_grad()
-            pred = model(tweets)
-            loss = criterion(pred, labels)
+            labels = labels[0]
+            tweet = preprocess(tweets[0])
+            # tweet = [tokenizer(t, return_tensors='pt') for t in tweets]
+            # labels = [l for l in labels]
+            print(tweet)
+            input = tokenizer(tweet, return_tensors='pt')
+            pred = model(**input)
+            loss = criterion(pred[0][0], torch.tensor(config.label2id[labels]))
             loss.backward()
             optimizer.step()
             cum_loss += loss.item()
-        
-
-
         print('Loss in epoch ' + str(e) + ': ' +
             str(cum_loss/len(train_set)))
         training_loss.append(cum_loss / len(train_set))
