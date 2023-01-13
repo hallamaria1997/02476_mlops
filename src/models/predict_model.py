20e7e9c650ed42e278a401d838961b7d30f31632
thordur16 <thordur16@ru.is>
2023-01-13 14:23:32 +0100

    added predict_model

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,62 +1,36 @@
-import click
-import matplotlib.pyplot as plt
-import torch
-from torch import nn
-from tqdm import tqdm
-from typing import Callable, Tuple, Union, Optional, List
-
-
-
-from src.models.load_data import make_dataloader
-from src.models.model import  SentimentModel    
-
-
-@click.group()
-def cli():
-    pass
-
+from model import SentimentModel
+import torch, click
+from load_data import make_dataloader
+from scipy.special import softmax
+import numpy as np
 
 @click.command()
-@click.option("--lr", default=3e-3, help="learning rate to use for training")
-@click.option("--epochs", default=50, help="number of epochs")
-def train(lr:float, epochs:int)->None:
-    """main training function for the model, calls the subsequent training function"""
-    model = SentimentModel()
-    train_set, _ = make_dataloader(data_path="data/processed/train_csv")
-    criterion = nn.NLLLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+@click.argument('model_path', default='models/checkpoint.pth')
+@click.argument('data_path', default='data/processed/test.csv')
+def predict(model_path: str, data_path: str):
+	"""
+		Uses the SentimentModel with pre-trained weights to acquire prediction
+		for a given data (data_path).
+	"""
+	if model_path[-4:] != '.pth':
+		model_path += '.pth'
+	
+	state_dict = torch.load(model_path)
+	test_set = make_dataloader(data_path)
+	model = SentimentModel()
+	model.load_state_dict(state_dict)
 
-    epochs = epochs
-    training_loss = []
+	correct, total = 0, 0
+	for tweets, att_mask, labels in test_set:		
+		preds = model(tweets, att_mask)
+		print(np.argmax(softmax(preds[0][0].detach().numpy())))
+		pred_labels = np.argmax(softmax(preds[0][0].detach().numpy()))
+		
+		correct += (pred_labels == labels).sum().item()
+		total += labels.numel()
 
-    for e in tqdm(range(epochs)):
-        cum_loss = 0
-        for tweets, labels in train_set:
-            optimizer.zero_grad()
-            pred = model(tweets)
-            loss = criterion(pred, labels)
-            loss.backward()
-            optimizer.step()
-            cum_loss += loss.item()
-        
+	print(labels)
+	print('Test set accuracy:', correct/total)
 
-
-        print('Loss in epoch ' + str(e) + ': ' +
-            str(cum_loss/len(train_set)))
-        training_loss.append(cum_loss / len(train_set))
-
-    torch.save(model.state_dict(), "models/checkpoint.pth")
-    print("saved to model/checkpoint.pth")
-
-    plt.figure(figsize=(10, 5))
-    plt.plot(loss)
-    plt.title("Training loss")
-    plt.xlabel("Epochs")
-    plt.ylabel("Loss")
-    plt.savefig("reports/figures/loss.png", dpi=200)
-
-
-cli.add_command(train)
-
-if __name__ == "__main__":
-    cli()
+if __name__ == '__main__':
+    predict()
