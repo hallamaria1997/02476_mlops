e4d52ca8da2c4a92f2da0c4821dc8f3e3debf740
thordur16 <thordur16@ru.is>
2023-01-16 16:11:46 +0100

    flake8 fixes

--- src/API/main.py
+++ src/API/main.py
@@ -5,13 +5,16 @@ from src.models.predict_model import Predict
 app = FastAPI()
 p = Predict()
 
+
 def save_tweet(tweet: str, pred_id: int, pred_label: str):
     with open('predictions.csv', 'a') as file:
         file.write('\n' + tweet + ',' + str(pred_id) + ',' + pred_label)
 
+
 @app.get("/")
 def root():
     return {"Hello": "World"}
+
 
 @app.get("/predict/{tweet}")
 def predict(tweet: str, background_tasks: BackgroundTasks):
@@ -19,4 +22,4 @@ def predict(tweet: str, background_tasks: BackgroundTasks):
     pred_id, pred_label = p.predict(tweet=tweet)
     background_tasks.add_task(save_tweet, tweet, pred_id, pred_label)
     return {"pred_id": str(pred_id),
-        "pred_label": pred_label}
+            "pred_label": pred_label}
