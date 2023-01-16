c67ebafdd58446b89fcb8d400cd89a665c984dad
thordur16 <thordur16@ru.is>
2023-01-16 16:10:22 +0100

    edited indentation

--- src/API/main.py
+++ src/API/main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI, BackgroundTasks
 from src.models.predict_model import Predict
-from typing import Tuple
 
 
 app = FastAPI()
 p = Predict()
 
 def save_tweet(tweet: str, pred_id: int, pred_label: str):
-	with open('predictions.csv', 'a') as file:
-		file.write('\n' + tweet + ',' + str(pred_id) + ',' + pred_label)
+    with open('predictions.csv', 'a') as file:
+        file.write('\n' + tweet + ',' + str(pred_id) + ',' + pred_label)
 
 @app.get("/")
 def root():
@@ -16,8 +15,8 @@ def root():
 
 @app.get("/predict/{tweet}")
 def predict(tweet: str, background_tasks: BackgroundTasks):
-	tweet = tweet.replace(',', '')
-	pred_id, pred_label = p.predict(tweet=tweet)
-	background_tasks.add_task(save_tweet, tweet, pred_id, pred_label)
-	return {"pred_id": str(pred_id),
-			"pred_label": pred_label}
+    tweet = tweet.replace(',', '')
+    pred_id, pred_label = p.predict(tweet=tweet)
+    background_tasks.add_task(save_tweet, tweet, pred_id, pred_label)
+    return {"pred_id": str(pred_id),
+        "pred_label": pred_label}
