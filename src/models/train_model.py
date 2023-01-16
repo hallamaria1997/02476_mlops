1f69ec8298874ee25d19b73e2963103814b989b3
Rebekka Johannsdottir <rebjohanns@gmail.com>
2023-01-16 16:28:31 +0100

    wandb init added

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -6,19 +6,23 @@ from omegaconf import DictConfig, OmegaConf
 from load_data import make_dataloader
 from model import SentimentModel
 import hydra
+import wandb
 
 
 @hydra.main(config_path="config", config_name='default_config.yaml')
 def train(config: DictConfig) -> None:
     """main training function for the model,
     calls the subsequent training function"""
+
     print(f"configuration: \n {OmegaConf.to_yaml(config)}")
-    hyparams = config.experiment
+    hyparams = config.experiment.hyper_parameters
     # torch.manual_seed(hyparams["seed"])
     epochs = hyparams['n_epochs']
     lr = hyparams['lr']
     batch_size = hyparams['batch_size']
     n_rows = hyparams['n_rows']
+
+    wandb.init(mode=config.experiment.wandb.mode)
 
     model = SentimentModel()
     train_set = make_dataloader(filepath="data/raw/train.csv",
@@ -30,6 +34,8 @@ def train(config: DictConfig) -> None:
     epochs = epochs
     training_loss = []
 
+
+    
     for e in tqdm(range(epochs)):
         cum_loss = 0
         for tweets, att_mask, labels in train_set:
@@ -43,7 +49,7 @@ def train(config: DictConfig) -> None:
         print('Loss in epoch ' + str(e) + ': ' + str(cum_loss/len(train_set)))
         training_loss.append(cum_loss / len(train_set))
 
-    torch.save(model.state_dict(), "models/checkpoint.pth")
+    torch.save(model.state_dict(), "C:/Users/Rebekka/Desktop/DTU/MLOps/02476_mlops/models/checkpoint.pth")
     print("saved to model/checkpoint.pth")
 
     plt.figure(figsize=(10, 5))
@@ -52,7 +58,7 @@ def train(config: DictConfig) -> None:
     plt.title("Training loss")
     plt.xlabel("Epochs")
     plt.ylabel("Loss")
-    plt.savefig("reports/figures/loss.png", dpi=200)
+    plt.savefig("C:/Users/Rebekka/Desktop/DTU/MLOps/02476_mlops/reports/figures/loss.png", dpi=200)
 
 
 if __name__ == "__main__":
