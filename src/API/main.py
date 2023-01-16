d5d5ab2cfe046a047ed3bee52d104e806e711f1d
thordur16 <thordur16@ru.is>
2023-01-16 15:46:02 +0100

    added background tasks

--- src/API/main.py
+++ src/API/main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
 from src.models.predict_model import Predict
+from typing import Tuple
+
 
 app = FastAPI()
 p = Predict()
 
+
 @app.get("/")
 def root():
-   return {"Hello": "World"}
+    return {"Hello": "World"}
+
 
 @app.get("/predict/{tweet}")
-def predict(tweet: str):
-	pred_id, pred_label = p.predict(tweet=tweet)
-	return {"pred_id": str(pred_id),
-			"pred_label": pred_label}
+def predict(tweet: str) -> Tuple[int, str]:
+    pred_id, pred_label = p.predict(tweet=tweet)
+    return {"pred_id": str(pred_id), "pred_label": pred_label}
