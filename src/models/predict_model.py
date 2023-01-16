40ced077d079363a10f959fe5258bcbb1ab8b59e
thordur16 <thordur16@ru.is>
2023-01-16 13:23:28 +0100

    put predict in class

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,34 +1,32 @@
-from model import SentimentModel
-import torch, click
+from src.models.model import SentimentModel
+import torch
 from scipy.special import softmax
 import numpy as np
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoConfig
 
-@click.command()
-@click.option('--model_path', default='models/checkpoint.pth')
-@click.option('--tweet', default='')
-def predict(model_path: str, tweet: str):
+class Predict():
 	"""
 		Uses the SentimentModel with pre-trained weights to acquire prediction
 		for a given data (data_path).
 	"""
-	if model_path[-4:] != '.pth':
-		model_path += '.pth'
-	
-	state_dict = torch.load(model_path)
-	model = SentimentModel()
-	model.load_state_dict(state_dict)
+	def __init__(self, tweet: str='', model_path:str = 'models/checkpoint.pth'):
+		if model_path[-4:] != '.pth':
+			model_path += '.pth'
+		state_dict = torch.load(model_path)
+		self.model = SentimentModel()
+		self.model.load_state_dict(state_dict)
+		self.tweet = tweet
 
-	tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
-	
-	tokens = tokenizer(tweet, padding=True, return_tensors='pt')
-	tweet_tokens = tokens.input_ids
-	att_mask = tokens.attention_mask
-	pred = model(tweet_tokens, att_mask)
+	def predict(self):	
+		tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
+		config = AutoConfig.from_pretrained( "cardiffnlp/twitter-roberta-base-sentiment-latest")
+		
+		tokens = tokenizer(self.tweet, padding=True, return_tensors='pt')
+		tweet_tokens = tokens.input_ids
+		att_mask = tokens.attention_mask
+		pred = self.model(tweet_tokens, att_mask)
 
-	pred_label = np.argmax(softmax(pred[0][0].detach().numpy()))
-	print(pred_label)
-	return pred_label
+		pred_id = np.argmax(softmax(pred[0][0].detach().numpy()))
+		pred_label = config.id2label[pred_id]
 
-if __name__ == '__main__':
-    predict()
+		return pred_id, pred_label
