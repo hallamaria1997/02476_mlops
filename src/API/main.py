df24ffc024fec0ba6106afe7f9ef9f768fc6ed2b
hallamh16 <50322630+hallamh16@users.noreply.github.com>
2023-01-16 15:39:15 +0100

    fixing indentation

--- src/API/main.py
+++ src/API/main.py
@@ -14,5 +14,5 @@ def root():
 
 @app.get("/predict/{tweet}")
 def predict(tweet: str) -> Tuple[int, str]:
-	pred_id, pred_label = p.predict(tweet=tweet)
-	return {"pred_id": str(pred_id), "pred_label": pred_label}
+    pred_id, pred_label = p.predict(tweet=tweet)
+    return {"pred_id": str(pred_id), "pred_label": pred_label}
