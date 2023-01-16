1b24c0005023444751729472baab026a9e7c68ce
hallamh16 <50322630+hallamh16@users.noreply.github.com>
2023-01-16 08:15:17 +0100

    commiting tests folder

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,4 +1,3 @@
-from transformers import AutoModelForSequenceClassification, AutoConfig, AutoTokenizer
 import click
 import matplotlib.pyplot as plt
 import torch
@@ -17,12 +16,12 @@ def cli():
 
 @click.command()
 @click.option("--lr", default=1e-4, help="learning rate to use for training")
-@click.option("--epochs", default=4, help="number of epochs")
+@click.option("--epochs", default=30, help="number of epochs")
 def train(lr:float, epochs:int)->None:
     """main training function for the model, calls the subsequent training function"""
     
     model = SentimentModel()
-    train_set = make_dataloader(filepath="data/raw/train.csv")
+    train_set = make_dataloader(filepath="C:/Users/Lenovo/Documents/02476_mlops/data/raw/train.csv")
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
@@ -43,7 +42,7 @@ def train(lr:float, epochs:int)->None:
             str(cum_loss/len(train_set)))
         training_loss.append(cum_loss / len(train_set))
 
-    torch.save(model.state_dict(), "models/checkpoint.pth")
+    torch.save(model.state_dict(), "C:/Users/Lenovo/Documents/02476_mlops/models/checkpoint.pth")
     print("saved to model/checkpoint.pth")
 
     plt.figure(figsize=(10, 5))
@@ -52,7 +51,7 @@ def train(lr:float, epochs:int)->None:
     plt.title("Training loss")
     plt.xlabel("Epochs")
     plt.ylabel("Loss")
-    plt.savefig("reports/figures/loss.png", dpi=200)
+    plt.savefig("C:/Users/Lenovo/Documents/02476_mlops/reports/figures/loss.png", dpi=200)
 
 
 cli.add_command(train)
---
+++ tests/test_data.py
@@ -0,0 +1,33 @@
+from __init__ import _PATH_DATA,_TEST_ROOT,_PROJECT_ROOT
+import sys
+sys.path.insert(1,'src/models')
+from load_data import make_dataloader
+import os.path
+import pytest
+
+train_path = 'data/raw/train.csv'
+test_path = 'data/raw/test.csv'
+N_train = 3200 # 100 batches of 32 observations
+N_test = 3200 # 100 batches of 32 observations
+batch_size = 32
+
+@pytest.mark.skipif(not os.path.exists(train_path), reason="Train data files not found")
+def test_train_data():
+    train_set = make_dataloader(train_path)
+    assert len(train_set) == N_train/batch_size, "Train Data: dataset did not have the correct number of samples"
+    for tweets, att_mask, labels in train_set:
+        assert tweets.size(dim=0)==batch_size and tweets.size(dim=1)==72, "Train Data: Size of tweets attributes are incorrect"
+        assert att_mask.size(dim=0)==batch_size and att_mask.size(dim=1)==72, "Train Data: Size of att_mask attribute is incorrect"
+        assert labels.size(dim=0)==32, "Train Data: Size of labels attribute is incorrect"
+        assert len(set(labels.tolist())), "Train Data: Not all batches hold all possible labels"
+
+@pytest.mark.skipif(not os.path.exists(test_path), reason="Test data files not found")
+def test_test_data():
+    test_set = make_dataloader(test_path)
+    assert len() == N_train/batch_size, "Test Data: dataset did not have the correct number of samples"
+    for tweets, att_mask, labels in test_set:
+        assert tweets.size(dim=0)==batch_size and tweets.size(dim=1)==72, "Test Data: Size of tweets attributes are incorrect"
+        assert att_mask.size(dim=0)==batch_size and att_mask.size(dim=1)==72, "Test Data: Size of att_mask attribute is incorrect"
+        assert labels.size(dim=0)==32, "Test Data: Size of labels attribute is incorrect"
+        assert len(set(labels.tolist())), "Test Data: Not all batches hold all possible labels"
+    
