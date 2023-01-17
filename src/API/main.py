d3867bf5e9eb06e943ec84794c6a0eb8362c8cc6
thordur16 <thordur16@ru.is>
2023-01-17 10:15:54 +0100

    Added documentation to main.py

--- src/API/main.py
+++ src/API/main.py
@@ -7,17 +7,26 @@ p = Predict()
 
 
 def save_tweet(tweet: str, pred_id: int, pred_label: str):
+    """Saves the received tweet to a database, along with the predicted
+    id and label."""
     with open('predictions.csv', 'a') as file:
         file.write('\n' + tweet + ',' + str(pred_id) + ',' + pred_label)
 
 
 @app.get("/")
 def root():
-    return {"Hello": "World"}
+    """The root."""
+    return {"message": "Call /predict/<tweet> to get a prediction."}
 
 
 @app.get("/predict/{tweet}")
 def predict(tweet: str, background_tasks: BackgroundTasks):
+    """Returns a classification of an input sentence.
+    Parameters: 
+        tweet (string). Inserted as a parameter in the URL.
+    Returns: 
+        pred_id (int): A numeral representation of the class (0, 1 or 2).
+        pred_label (string): A text representation of the class (negative, neutral, positive)."""
     tweet = tweet.replace(',', '')
     pred_id, pred_label = p.predict(tweet=tweet)
     background_tasks.add_task(save_tweet, tweet, pred_id, pred_label)
